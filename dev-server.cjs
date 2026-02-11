b5ae3a0584dd3bb57714f0240721277c3a018669
sourmera <sourmera@gmail.com>
2026-02-11 09:15:17 +0200

    Update dev-server.cjs

--- dev-server.cjs
+++ dev-server.cjs
@@ -23,10 +23,10 @@ app.post('/api/contact', async (req, res) => {
   try {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
-      port: 465,
-      secure: true,
-      auth: { user, pass },
-      tls: { rejectUnauthorized: false }
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
     });
 
     await transporter.sendMail({
